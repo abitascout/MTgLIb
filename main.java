174a2701d86247b62cd1656be28f1adad086cc78
AetherialAvenger <matthew.d.gerber@gmail.com>
2018-10-30 17:20:08 -0500

    Main

--- main.java
+++ main.java
@@ -4,23 +4,28 @@ import java.io.FileNotFoundException;
 public class main {
 	public static void main (String args[])
 	{
+		
+		importFile();
+		Card q = new Card();
+		q.setCmc("1B");
+		q.setPrice(1.14);
+		System.out.println(q);
+	}
+	public static void importFile()
+	{
 		File in = new File("input.txt");
 		try
 		{
 			Scanner FileReader = new Scanner(in);
 			String x = FileReader.next();
 			System.out.println(x);
+			FileReader.close();
 		}
 		catch(FileNotFoundException e)
 		{
 			System.out.println("Data File Missing.\nShutting down..");
 			System.exit(0);
 		}
-		
-		Card x = new Card();
-		x.setCmc("1B");
-		x.setPrice(1.14);
-		System.out.println(x);
 	}
 
 }
