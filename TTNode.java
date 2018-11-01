a6f42f71d61f9c3bf3f682afe59c3cd690b588f1
AetherialAvenger <matthew.d.gerber@gmail.com>
2018-10-31 21:22:28 -0500

    Fixed Two Three Tree Skeleton

---
+++ TTNode.java
@@ -0,0 +1,56 @@
+public class TTNode<Type>
+{
+	private Type Ldata,Rdata;
+	private TTNode<Type> Llink,Mlink,Rlink;
+	
+	// constructor
+	public TTNode()
+	{
+		this.Ldata = this.Rdata = null;
+		this.Llink = this.Rlink = null;
+	}
+	
+	// acccessor and mutator for the data 
+	public void set_rightData(Type data)
+	{
+		this.Rdata = data;
+	}
+	public Type get_rightData()
+	{
+		return this.Rdata;
+	}
+	public void set_leftData(Type data)
+	{
+		this.Ldata = data;
+	}
+	public Type get_leftData()
+	{
+		return this.Ldata;
+	}
+	
+	// accessor and mutator for the link
+	public void set_leftLink(TTNode<Type> link)
+	{
+		this.Llink = link;
+	}
+	public TTNode<Type> get_leftLink()
+	{
+		return this.Llink;
+	}
+	public void set_rightLink(TTNode<Type> link)
+	{
+		this.Rlink = link;
+	}
+	public TTNode<Type> get_rightLink()
+	{
+		return this.Rlink;
+	}
+	public void set_middleLink(TTNode<Type> link)
+	{
+		this.Mlink = link;
+	}
+	public TTNode<Type> get_middle()
+	{
+		return this.Mlink;
+	}
+}
---
+++ TTtree.java
@@ -0,0 +1,21 @@
+public class TTtree<Type>
+{
+	
+	private TTNode<Type> root;
+	// constructor
+	// empty list has a size of -1 and its position is at -1
+	public TTtree()
+	{
+		this.root = null;
+	}
+	// copy constructor
+	// clones the list l 
+	public TTtree(TTtree<Type> l)
+	{
+		TTNode<Type> n = l.root;
+		this.root = null;
+	}
+	
+	// this is far as I have gotten without doing som research
+	
+}
