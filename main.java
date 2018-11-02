42f76ea8271694689598f67711f2126fe2f51aac
Dragonborn <abitascoutrobert@gmail.com>
2018-11-02 16:33:57 -0500

    made a output command

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.PrintStream;
 public class main {
 	public static void main (String args[])
 	{
@@ -9,6 +11,7 @@ public class main {
 		Card q = new Card("adj;klf","1B","pussy",1.24);
 		TTtree<Card> library = new TTtree<Card>();
 		System.out.println(q);
+		exportFile();
 	}
 	public static void importFile()
 	{
@@ -35,6 +38,25 @@ public class main {
 			System.out.println("Data File Missing.\nShutting down..");
 			System.exit(0);
 		}
+	}
+	public static void exportFile()
+	{
+		// Create a PrintStream attached to a file named "output.txt"
+		// This will overwrite  the file if its already extis
+		PrintStream ps = new PrintStream("output.txt");
+		// Buffer some data to write to the file (doesnt write till its flu
+		ps.print("Some test data");
+		
+		// Flush all buffered data to the file
+		ps.flush();
+		
+		ps.println("data");
+		
+		// Close the file (by closing the PrintStream)
+		// also flushes any remainin buffered output
+		ps.close();
+		
+		
 	}
 
 }
