5a32b2da3d4c53b59e5ac0b4d288d9f2e2e9bb73
AetherialAvenger <matthew.d.gerber@gmail.com>
2018-11-05 09:29:12 -0600

    ttnode

--- TTNode.java
+++ TTNode.java
@@ -61,6 +61,7 @@ public class TTNode<Type>
 			this.Children.GetValue().getVals(iteratorList);
 		}
 	}
+	//recursively adds to a subtree
 	public boolean add(Type x)
 	{
 		if(isLeaf())
@@ -72,16 +73,62 @@ public class TTNode<Type>
 			return addToInterior(x);
 		}
 	}
-	public boolean addToLeaf(Card x)
+	//new values are always added to a leaf. the result may be a 4-node leaf.
+	public boolean addToLeaf(Type x)
 	{
 		int cmp;
 		//size is 1 for a 2 node, or 2 for a 3 node
 		for(int i = 0; i < this.Data.GetSize(); i++)
 		{
 			cmp = x.toString().compareTo(this.Data.GetValue().toString());
-			
+			if (cmp == 0)
+			{
+				return false;
+			}
+			else if(cmp < 0)
+			{
+				this.Data.SetPos(i);
+				this.Data.InsertAfter(x);
+				return true;
+			}
+			this.Data.Next();
 		}
+		this.Data.InsertAfter(x);
+		return true;
 	}
+	//adds a value to a subtree. if the addition results in a 4-node then adjustments are made.
+	boolean addToInterior(Type x)
+	{
+		int cmp;
+		//size 1 = 2-node size 2 = 3 node
+		for(int i = 0; i<=this.Data.GetSize(); i++)
+		{
+			if(i == this.Data.GetSize())
+			{
+				cmp = -1; //because there is no Data[2]
+			}
+			else
+			{
+				this.Data.SetPos(i);
+				cmp = x.toString().compareTo(this.Data.GetValue().toString());
+			}
+			if(cmp == 0)
+			{
+				return false;
+			}
+			else if(cmp < 0)
+			{
+				this.Children.SetPos(i);
+				this.Children.GetValue().InsertAfter(x);
+				boolean retVal = 
+				if(Children.GetValue().is4Node())
+				{
+					childIs4Node(i);
+				}
+				return retVal;
+			}
+		}
+		return false; //unreachable but compiler complains
 	}
 	//boolean functions
 	public boolean isLeaf()
